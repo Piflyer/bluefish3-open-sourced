c94b68aa9f88c5f6bc57d991dfab008b66e07779
Phineas Scovel <62225152+pitfall24@users.noreply.github.com>
2022-07-11 19:38:41 -0400

    corrected file name

--- automate.sh
+++ automate.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
-python3 test.py "schedule.json"
-#Ouput to desired folder
+# Run main on desired schedule json file
+python3 main.py "schedule.json"
+# Ouput to desired folder
 mv output output_control_day_x
